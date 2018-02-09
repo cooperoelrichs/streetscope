11bc9cb17c18eae09fbbbf8fcc7dd4cac9bfc65b
cooperoelrichs <c.oelrichs@gmail.com>
2018-02-09 14:43:10 +1100

    Formatting.

--- index_addresses.py
+++ index_addresses.py
@@ -6,34 +6,50 @@ from urllib.request import urlparse
 from elasticsearch import Elasticsearch
 
 if os.environ.get('BONSAI_URL'):
-  url = urlparse(os.environ['BONSAI_URL'])
-  ELASTICSEARCH_HOST = url.hostname
-  ELASTICSEARCH_AUTH = url.username + ':' + url.password
-  es = Elasticsearch([{'host': ELASTICSEARCH_HOST}], http_auth=ELASTICSEARCH_AUTH)
+    url = urlparse(os.environ['BONSAI_URL'])
+    ELASTICSEARCH_HOST = url.hostname
+    ELASTICSEARCH_AUTH = url.username + ':' + url.password
+    es = Elasticsearch(
+        [{'host': ELASTICSEARCH_HOST}], http_auth=ELASTICSEARCH_AUTH
+    )
 else:
-  es = Elasticsearch()
+    es = Elasticsearch()
 
 files_given = sys.argv
 for file_name in files_given:
-  if file_name == 'index_addresses.py':
-    continue
-  else:
-    file_path = file_name
-    print('adding ' + file_path)
+    if file_name == 'index_addresses.py':
+        continue
+    else:
+        file_path = file_name
+        print('adding ' + file_path)
 
-    with open(file_path, 'r') as csvfile:
-      print('open file')
-      csv_reader = csv.DictReader(csvfile, fieldnames=[], restkey='undefined-fieldnames', delimiter=',')
+        with open(file_path, 'r') as csvfile:
+            print('open file')
+            csv_reader = csv.DictReader(
+                csvfile, fieldnames=[], restkey='undefined-fieldnames',
+                delimiter=','
+            )
 
-      current_row = 0
-      for row in csv_reader:
-        current_row += 1
-        if current_row == 1:
-          csv_reader.fieldnames = row['undefined-fieldnames']
-          continue
-        address = row
-        if current_row % 1000 == 0:
-          print('%s addresses indexed' % current_row)
-        es.index(index='addresses', doc_type='address', id=current_row-1, body={'NUMBER': address[' NUMBER'], 'STREET': address[' STREET'], 'ADDRESS': address[' NUMBER'] + ' ' + address[' STREET'], 'X': address['LON'], 'Y': address[' LAT']})
+            current_row = 0
+            for row in csv_reader:
+                current_row += 1
+                if current_row == 1:
+                    csv_reader.fieldnames = row['undefined-fieldnames']
 
-    csvfile.close()
+                address = row
+                if current_row % 1000 == 0:
+                    print('%s addresses indexed' % current_row)
+
+                es.index(
+                    index='addresses',
+                    doc_type='address',
+                    id=current_row-1,
+                    body={
+                        'NUMBER': address[' NUMBER'],
+                        'STREET': address[' STREET'],
+                        'ADDRESS': address[' NUMBER'] + ' ' + address[' STREET'],
+                        'X': address['LON'],
+                        'Y': address[' LAT']}
+                    )
+
+        csvfile.close()
